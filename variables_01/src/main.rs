61b50d9aba550e8bf22f3b07c922d660cdb3b3a9
hanapiko <apikojuma94@gmail.com>
2025-04-15 10:57:56 +0300

    variables_01

--- variables_01/src/main.rs
+++ variables_01/src/main.rs
@@ -18,3 +18,15 @@ fn main() {
         println!("x is not ten!");
     }
 }
+
+// fn main() {
+//     // TODO: Change the line below to fix the compiler error.
+//     let x: i32;
+
+//     println!("Number {x}");
+// }
+fn main() {
+    let x: i32 = 3;
+
+    println!("Number {x}");
+}
