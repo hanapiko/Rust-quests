93ac6f04a0437e69aef9f51832165b05ebabc88a
hanapiko <apikojuma94@gmail.com>
2025-04-15 10:45:00 +0300

    variable_01

---
+++ variables_01/src/main.rs
@@ -0,0 +1,20 @@
+// fn main() {
+//     // TODO: Change the line below to fix the compiler error.
+//     let x;
+
+//     if x == 10 {
+//         println!("x is ten!");
+//     } else {
+//         println!("x is not ten!");
+//     }
+// }
+
+fn main() {
+    let x = 10;
+
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
